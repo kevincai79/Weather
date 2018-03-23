8ed4015c1afeac18655ac7bc183bf93951449b0c
Kevin Cai <kevincai1979@gmail.com>
2018-03-23 13:28:18 -0700

    check type in city name in the reducer_weather file to ensure correct city name input

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,7 +5,12 @@ export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
       // return state.concat([action.payload.data]);
-      return [action.payload.data, ...state]; // ES6 sytax, same result as above line
+      if (action.payload.data) {
+        return [action.payload.data, ...state]; // ES6 sytax, same result as above line
+      } else {
+        alert('please type in correct full city name');
+      }
   }
+
   return state;
 }
